55d6a56a298acc5d906abe73f5daa6241761c9d2
Senmakana <123734431+Senmakana@users.noreply.github.com>
2024-12-27 15:41:13 +0300

    Add service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,54 +1,22 @@
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('app-cache').then((cache) => {
-      return cache.addAll([
-        '/Mywebapp/', // Add the relative root of your app
-        '/Mywebapp/index.html',
-        '/Mywebapp/style.css',
-        '/Mywebapp/converters/index.html',
-        '/Mywebapp/converters/distance/index.html',
-        '/Mywebapp/quantities/index.html',
-        '/Mywebapp/quantities/wallarea/index.html',
-        '/Mywebapp/quantities/wallarea/fenestrations/index.html',
-        '/Mywebapp/quantities/wallarea/fenestrations/passages/index.html',
-        '/Mywebapp/quantities/wallarea/fenestrations/windows/index.html',
-        '/Mywebapp/quantities/wallarea/fenestrations/doors/index.html',
-        '/Mywebapp/quantities/wallarea/walldimensions/index.html',
-        '/Mywebapp/quantities/concretevolume/index.html',
-        '/Mywebapp/quantities/concretevolume/otherconcretestructures/index.html',
-        '/Mywebapp/quantities/concretevolume/staircase/index.html',
-        '/Mywebapp/materials/index.html',
-        '/Mywebapp/materials/roof/index.html',
-        '/Mywebapp/materials/roof/roofstructure/index.html',
-        '/Mywebapp/materials/roof/roofstructure/timberrafters/index.html',
-        '/Mywebapp/materials/roof/roofstructure/timberpurlins/index.html',
-        '/Mywebapp/materials/roof/roofingmaterials/index.html',
-        '/Mywebapp/materials/roof/roofingmaterials/Corrugatedironsheets/index.html',
-        '/Mywebapp/materials/roof/roofdimensions/index.html',
-        '/Mywebapp/materials/concrete/index.html',
-        '/Mywebapp/materials/concrete/concretemix/index.html',
-        '/Mywebapp/materials/wall/index.html',
-        '/Mywebapp/materials/wall/morta/index.html',
-        '/Mywebapp/materials/wall/plaster/index.html',
-        '/Mywebapp/materials/wall/block/index.html',
-        '/Mywebapp/materials/rebar/columnrebar/index.html',
-        '/Mywebapp/materials/rebar/columnrebar/columnrings/index.html',
-        '/Mywebapp/materials/rebar/columnrebar/columnmains/index.html',
-        '/Mywebapp/materials/rebar/beamrebar/index.html',
-        '/Mywebapp/materials/rebar/beamrebar/beamrings/index.html',
-        '/Mywebapp/materials/rebar/beamrebar/beammains/index.html',
-        '/Mywebapp/materials/rebar/slabrebar/index.html',
-        '/Mywebapp/materials/rebar/slabrebar/slabrings/index.html',
-        '/Mywebapp/materials/rebar/index.html',
-      ]);
-    })
-  );
+    event.waitUntil(
+        caches.open('safaricom-cache').then((cache) => {
+            return cache.addAll([
+                '/',
+                '/index.html',
+                '/manifest.json',
+                '/icon.png',
+                '/styles.css',  // Include any other files you want to cache
+                '/scripts.js'
+            ]);
+        })
+    );
 });
 
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
 });
