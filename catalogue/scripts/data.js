388d3220df0db534c4b60e80dd34bd1e8c2f08f0
Senmakana <123734431+Senmakana@users.noreply.github.com>
2025-02-18 14:29:27 +0300

    Add catalogue/scripts/data.js

---
+++ catalogue/scripts/data.js
@@ -0,0 +1,28 @@
+// data.js
+
+// Materials data
+const materials = [
+    { name: 'cement', unit: 'bags' },
+    { name: 'sand', unit: 'tons' },
+    { name: 'ballast', unit: 'tons' },
+    { name: '400x200x200', unit: 'pcs' },
+    { name: '360x180x180', unit: 'pcs' },
+    { name: '300x150x150', unit: 'pcs' },
+    { name: 'D8', unit: 'pcs' },
+    { name: 'D10', unit: 'pcs' },
+    { name: 'D12', unit: 'pcs' },
+    { name: 'D16', unit: 'pcs' },
+    { name: 'D20', unit: 'pcs' },
+    { name: 'D25', unit: 'pcs' },
+    { name: 'D32', unit: 'pcs' },
+    { name: 'binding wire', unit: 'kg' }
+];
+
+// Currencies data
+const currencies = [
+    { symbol: '£', name: 'GBP' },
+    { symbol: '€', name: 'EUR' },
+    { symbol: '¥', name: 'JPY' },
+    { symbol: '$', name: 'USD' },
+    { symbol: 'Ksh', name: 'KES' }
+];
